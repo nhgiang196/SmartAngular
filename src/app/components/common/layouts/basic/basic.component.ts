bf4e748a90f76ec640410bacfd240f3d422bd9f4
giang.nh <giang.nh@becamex.com.vn>
2020-01-14 14:43:45 +0700

    basic component

--- src/app/components/common/layouts/basic/basic.component.ts
+++ src/app/components/common/layouts/basic/basic.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { detectBody,smoothlyMenu } from '../../../../app.helpers';
+import { detectBody,smoothlyMenu, collapseIboxHelper } from '../../../../app.helpers';
 declare let $: any;
 @Component({
   selector: 'basic',
@@ -140,36 +140,8 @@ export class BasicComponent implements OnInit {
   }
 
   ngAfterViewInit(){
-    // Collapse ibox function
-    $('.collapse-link').on('click', function () {
-      var ibox = $(this).closest('div.ibox');
-      var button = $(this).find('i');
-      var content = ibox.children('.ibox-content');
-      content.slideToggle(200);
-      button.toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
-      ibox.toggleClass('').toggleClass('border-bottom');
-      setTimeout(function () {
-        ibox.resize();
-        ibox.find('[id^=map-]').resize();
-      }, 50);
-    });
-
-    // Close ibox function
-    $('.close-link').on('click', function () {
-      var content = $(this).closest('div.ibox');
-      content.remove();
-    });
-    // Fullscreen ibox function
-    $('.fullscreen-link').on('click', function () {
-      var ibox = $(this).closest('div.ibox');
-      var button = $(this).find('i');
-      $('body').toggleClass('fullscreen-ibox-mode');
-      button.toggleClass('fa-expand').toggleClass('fa-compress');
-      ibox.toggleClass('fullscreen');
-      setTimeout(function () {
-        $(window).trigger('resize');
-      }, 100);
-    });
+    collapseIboxHelper()
+    
     
   }
 
