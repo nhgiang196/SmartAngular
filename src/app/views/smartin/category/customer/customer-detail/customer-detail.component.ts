f5ed1f57d2ee2084a49824c0bf1112afe19e2ae9
giang.nh <giang.nh@becamex.com.vn>
2020-03-13 09:42:14 +0700

    Customer/Contract: upgrade upload Component + fix modal behavior

--- src/app/views/smartin/category/customer/customer-detail/customer-detail.component.ts
+++ src/app/views/smartin/category/customer/customer-detail/customer-detail.component.ts
@@ -11,6 +11,8 @@ import { AuthService } from 'src/app/services/auth.service';
 import { HttpEventType } from '@angular/common/http';
 import { setQuarter } from 'ngx-bootstrap/chronos/units/quarter';
 import { ContractComponent } from '../contract/contract.component';
+import { SmartUploadComponent } from '../../../ui-sample/smart-upload/smart-upload.component';
+import { async } from '@angular/core/testing';
 declare let $: any;
 @Component({
   selector: 'app-customer-detail',
@@ -19,6 +21,7 @@ declare let $: any;
 })
 export class CustomerDetailComponent implements OnInit {
   @ViewChild(ContractComponent) childView: ContractComponent;
+  @ViewChild('customerFile') uploadComponent: SmartUploadComponent;
 
   constructor(
     private api: WaterTreatmentService,
@@ -29,12 +32,9 @@ export class CustomerDetailComponent implements OnInit {
     private trans: TranslateService,
     private auth: AuthService
   ) { }
-  private pathFile = "uploadFileCustomer";
+  pathFile = "uploadFileCustomer";
   entity: Customer;
-  files: File[] = [];
-  addFiles: { FileList: File[], FileLocalNameList: string[] };
   invalid: any = { Existed_CustomerName: false, Null_CustomerName: false };
-  uploadReportProgress: any = { progress: 0, message: null, isError: null };
   initCombobox = { Factories: [], FullFactories: [] };
   EditRowNumber = 0;
   laddaSubmitLoading = false;
@@ -43,6 +43,7 @@ export class CustomerDetailComponent implements OnInit {
   /**INIT FUNCTIONS */
   ngOnInit() {
     this.resetEntity();
+    this.childView.resetEntity();
     this.loadInit();
   }
   async loadInit() {
@@ -54,12 +55,10 @@ export class CustomerDetailComponent implements OnInit {
       if (_factoryAddTag && await !this.initCombobox.Factories.find(x => x.FactoryId == dataResolver.FactoryId))
         this.initCombobox.Factories = this.initCombobox.Factories.concat([_factoryAddTag]);
       this.entity = dataResolver;
+      this.uploadComponent.loadInit(dataResolver.CustomerFile)
     }
     // await this.loadContractByCustomer();
-    this.entity.CustomerFile.forEach(item => {
-      let _tempFile = new File([], item.File.FileOriginalName);
-      this.files.push(_tempFile);
-    })
+    
     console.log(this.entity);
   }
   /**INIT FUNCTIONS */
@@ -71,10 +70,7 @@ export class CustomerDetailComponent implements OnInit {
   }
   private async resetEntity() { //reset entity values
     this.entity = new Customer();
-    this.files = [];
-    this.addFiles = { FileList: [], FileLocalNameList: [] }
     this.invalid = {};
-    this.uploadReportProgress = { progress: 0, message: null, isError: null };
     this.EditRowNumber = 0;
     this.editIndex = 0;
   }
@@ -84,50 +80,50 @@ export class CustomerDetailComponent implements OnInit {
     this.laddaSubmitLoading = true;
     let _valid = await this.api.validateCustomer(this.entity).toPromise().then() as any;
     if (!_valid.Success) {
+      $('#tab1_Home').click();
       this.laddaSubmitLoading = false;
       this.invalid[_valid.Message] = true;
-      return;
+      return false;
     }
     else {
       var e = this.entity;
-      if (await this.fnValidate(e)) {
-        if (this.route.snapshot.params.id == null) { //add
-          e.CreateBy = this.auth.currentUser.Username;
-          this.api.addCustomer(e).subscribe(res => {
-            var operationResult: any = res
-            if (operationResult.Success) {
-              this.toastr.success(this.trans.instant("messg.add.success"));
-              this.router.navigate(["/category/customer/" + operationResult.Data]);
-            }
-            else this.toastr.warning(operationResult.Message);
-            this.laddaSubmitLoading = false;
-          }, err => { this.toastr.error(err.statusText); this.laddaSubmitLoading = false; })
-        }
-        else {
-          e.ModifyBy = this.auth.currentUser.Username;
-          this.api.updateCustomer(e).subscribe(res => {
-            var operationResult: any = res
-            if (operationResult.Success) {
-              if (this.addFiles.FileList.length > 0) this.uploadFile(this.addFiles.FileList);
-              this.toastr.success(this.trans.instant("messg.update.success"));
-              this.router.navigate(["/category/customer/" + this.entity.CustomerId]);
-            }
-            else this.toastr.warning(operationResult.Message);
-            this.laddaSubmitLoading = false;
-          }, err => { this.toastr.error(err.statusText); this.laddaSubmitLoading = false; })
-        }
+      await this.uploadComponent.uploadFile();
+      if (this.route.snapshot.params.id == null) { //add
+        e.CreateBy = this.auth.currentUser.Username;
+        this.api.addCustomer(e).subscribe(res => {
+          var operationResult: any = res
+          if (operationResult.Success) {
+            this.toastr.success(this.trans.instant("messg.add.success"));
+            this.router.navigate(["/category/customer/" + operationResult.Data]);
+          }
+          else this.toastr.warning(operationResult.Message);
+          this.laddaSubmitLoading = false;
+        }, err => { this.toastr.error(err.statusText); this.laddaSubmitLoading = false; })
       }
+      else {
+        e.ModifyBy = this.auth.currentUser.Username;
+        this.api.updateCustomer(e).subscribe(res => {
+          var operationResult: any = res
+          if (operationResult.Success) {
+            this.toastr.success(this.trans.instant("messg.update.success"));
+            this.router.navigate(["/category/customer/" + this.entity.CustomerId]);
+          }
+          else this.toastr.warning(operationResult.Message);
+          this.laddaSubmitLoading = false;
+        }, err => { this.toastr.error(err.statusText); this.laddaSubmitLoading = false; })
+      }
+      return true;
     }
-  }
-  fnValidate(e) {
-    return true;
+    
+    
   }
   fnEditItem(contractId, index) {
-    this.childView.resetEntity();
+    if (this.entity.CustomerId!=0) this.childView.resetEntity();
     console.log('edit item', contractId);
     console.log('edit index', index)
     this.editIndex = index;
     this.app_contractId = contractId;
+    this.childView.loadInit(contractId)
   }
   fnDeleteItem(index) {
     swal.fire({
@@ -145,81 +141,12 @@ export class CustomerDetailComponent implements OnInit {
     })
   }
   /**EVENT TRIGGER */
-  async onSelect(event) { //drag file(s) or choose file(s) in ngFileZone
-    var askBeforeUpload = false;
-    if (event.rejectedFiles.length > 0) this.toastr.warning(this.trans.instant('messg.maximumFileSize5000'));
-    var _addFiles = event.addedFiles;
-    for (var index in _addFiles) {
-      let item = event.addedFiles[index];
-      let convertName = this.helper.getFileNameWithExtension(item);
-      let currentFile = this.entity.CustomerFile;
-      let findElement = currentFile.filter(x => x.File.FileOriginalName == item.name)[0];
-      //ASK THEN GET RESULT
-      if (findElement != null) {
-        if (!askBeforeUpload) {
-          askBeforeUpload = true;
-          var allowUpload = true;
-          await swal.fire({
-            title: 'File trùng',
-            titleText: 'Một số file bị trùng, bạn có muốn đè các file này lên bản gốc?',
-            type: 'warning',
-            showCancelButton: true,
-            reverseButtons: true
-          }).then((result) => {
-            if (result.dismiss === swal.DismissReason.cancel) allowUpload = false;
-          })
-        }
-        if (!allowUpload) return;
-        let _FileElement = this.files.filter(x => x.name == findElement.File.FileOriginalName)[0];
-        let _indexFileElement = this.files.indexOf(_FileElement, 0);
-        this.files.splice(_indexFileElement, 1);
-        this.addFiles.FileList.splice(_indexFileElement, 1);
-      }
-      else {
-        let _warehouseFile = new CustomerFile();
-        _warehouseFile.File.FileOriginalName = item.name;
-        _warehouseFile.File.FileLocalName = convertName;
-        _warehouseFile.File.Path = this.pathFile + '/' + convertName;
-        _warehouseFile.File.FileType = item.type;
-        this.entity.CustomerFile.push(_warehouseFile);
-        this.addFiles.FileLocalNameList.push(convertName);
-      }
-    }
-    this.files.push(...event.addedFiles); //refresh showing in Directive
-    this.addFiles.FileList.push(...event.addedFiles);
-  }
-  fnDownloadFile(filename) { //press FILES preview
-    this.api.downloadFile(this.pathFile + '/' + filename);
-  }
-  fnRemoveFile(event) { //PRESS X TO REMOVE FILES
-    let index = this.files.indexOf(event);
-    this.files.splice(index, 1); //UI del
-    this.entity.CustomerFile.splice(index, 1);
-  }
+ 
   onSwitchStatus(_TYPE) { //modal switch on change
     if (_TYPE == 'entity') this.entity.Status = this.entity.Status == 0 ? 1 : 0;
     if (_TYPE == 'entityIsIntergration') this.entity.IsIntergration = !this.entity.IsIntergration;
   }
-  private uploadFile(files: File[]) { //upload file to server
-    let formData = new FormData();
-    for (let index = 0; index < files.length; index++) {
-      let _file = files[index];
-      formData.append("files", _file, this.addFiles.FileLocalNameList[index]);
-    }
-    this.api.uploadFile(formData, this.pathFile).subscribe(event => {
-      if (event.type === HttpEventType.UploadProgress)
-        this.uploadReportProgress.progress = Math.round(100 * event.loaded / event.total);
-      else if (event.type === HttpEventType.Response) {
-        this.uploadReportProgress.message = this.trans.instant('Upload.UploadFileSuccess');
-        // this.onUploadFinished.emit(event.body);
-      }
-    }, err => {
-      this.toastr.warning(err.statusText, this.trans.instant('Upload.UploadFileError'));
-      this.uploadReportProgress = { progress: 0, message: 'Error: ' + err.statusText, isError: true };
-    });
-  }
-  
-  onChangeAdd(returnContract: Contract) {
+  async onChangeAdd(returnContract: Contract) {
     console.log('return Contract', returnContract);
     if (returnContract.CustomerId==0)
       swal.fire({
@@ -229,10 +156,11 @@ export class CustomerDetailComponent implements OnInit {
         type: 'question',
         showCancelButton: true,
         reverseButtons: true
-      }).then((result) => {
+      }).then(async (result)=>{
         if (result.value) {
           this.entity.Contract.splice(this.editIndex, 1, returnContract);
-          this.fnSave();
+          let saveValidate = await this.fnSave();
+          if (!saveValidate) this.entity.Contract = [];
         }
       })
     else {
