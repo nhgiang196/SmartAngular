b8274a8de0f7b6289153c2be23333bb7e7b0c50c
Hoang.cdt <hoang.cdt@becamex.com.vn>
2020-03-31 13:43:37 +0700

    commit

--- src/app/views/pages/categories/item-type/item-type.component.ts
+++ src/app/views/pages/categories/item-type/item-type.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-
-
 import { ItemTypeService, AuthService, ItemTypePropertyService } from 'src/app/core/services';
 import { DxDataGridComponent } from 'devextreme-angular';
 import config from 'devextreme/core/config';
@@ -9,6 +7,9 @@ import { ToastrService } from 'ngx-toastr';
 import DataGrid from "devextreme/ui/data_grid";
 import Swal from 'sweetalert2';
 import { ItemTypeProperty } from 'src/app/core/models/item';
+import { HttpParams } from '@angular/common/http';
+import DataSource from 'devextreme/data/data_source';
+import { async } from '@angular/core/testing';
 @Component({
   selector: 'app-item-type',
   templateUrl: './item-type.component.html',
@@ -21,8 +22,8 @@ export class ItemTypeComponent implements OnInit {
   dataSourceItemTypes: any;
   itemTypeId : number =0;
   itemTypeProperties : ItemTypeProperty []= []
-  dataSourceProperties: any;
-  entity: any
+  dataSourceProperties: any = {};
+  detail: any
   detailDelete: any;
 
   selectedRowIndex = -1;
@@ -39,15 +40,113 @@ export class ItemTypeComponent implements OnInit {
     });
   }
   ngOnInit() { }
-  /**
-   * Function Insert
-   * @param e with e is params in DevExtreme
-   * all Field in ItemProperty
-   * with ItemPropertyName
-   */
-  async onRowInsertingProperty(e) {
-    console.log(e);
-    e.data.ItemTypeId = this.itemTypeId;
+
+//Load popup by ID
+  async filterByItemTypeId(e){
+    this.itemTypeId = e.data.ItemTypeId;
+    let out_Data;
+    this.dataSourceProperties = 
+      //this.itemTypePropertyService.getDataGridItemTypePropertyByItemTypeId(this.dataSourceProperties, "ItemTypePropertyId", this.itemTypeId);
+      this.itemTypePropertyService.
+              getTest(this.dataSourceProperties, "ItemTypePropertyId", this.itemTypeId,out_Data);
+    setTimeout(()=>{
+      console.log(this.dataSourceProperties); 
+    },2000)
+    
+  }
+ 
+/////MASTER////////////////////////
+//Update Master
+  onRowUpdatingItemType(e)
+  { 
+    this.entity= this.dataSourceProperties._items
+    console.log(this.entity); 
+  }
+
+////DETAIL/////////////////
+//
+  onInitialized()
+  {
+    console.log(this.dataSourceProperties._items); 
+  }
+//Insert Detail
+  async onRowInsertingProperty(e)
+  {
+  //   const data = Object.assign(e.oldData, e.newData);
+  //   data.ModifyBy = this.auth.currentUser.Username;
+  //   data.Status = data.Status ? 1 : 0; //tenary operation if (data.status == true) return 1 else return 0
+  //   //data.ItemTypeProperty = this.itemTypeProperties;// đây là khi lưu cha con nó lưu luôn
+  //   //console.log('Save Master detail')
+  //   console.log(data);
+  //   let validateResult: any = await this.onValidateItemTypeName(data)
+  //   if (!validateResult.Success)
+  //     this.toastr.error('ItemTypeName already exsited!', 'Error!');
+  //   else {
+  //     this.itemTypeService.updateItemType(data).subscribe(res => {
+
+  //       const result = res as any;
+  //       if (result.Success) {
+  //         this.toastr.success('Update success!', 'Success!');
+  //         this.dataGrid.instance.refresh();
+  //       } else {
+  //         Swal.fire('Error!', result.Message, 'error');
+  //       }
+  //     });
+  //   console.log(this.dataSourceProperties._items); 
+  // }
+}
+//Update Detail
+  onRowUpdatingProperty(e)
+  {
+    console.log(this.dataSourceProperties._items); 
+     //Modify entity olddata to newdata;
+   const data = Object.assign(e.oldData, e.newData);
+    console.log(data);
+    this.itemTypePropertyService.updateItemTypeProperty(data).subscribe(res => {
+      const result = res as any;
+      if (result.Success) {
+        this.toastr.success('Update success!', 'Success!');
+        // this.dataGrid.instance.refresh();
+      } else {
+        Swal.fire('Error!', result.Message, 'error');
+      }
+    });
+
+    console.log(this.dataSourceProperties._items); 
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //async onRowInsertingProperty(e) {
+    //console.log(e.data);
+    //e.data.ItemTypeId = this.itemTypeId;
+    //e.data.ItemTypePropertyId = 0;
+    //this.itemTypeProperties.push(e.data)// thằng con owrd đây
+    //this.dataSourceProperties.push(this.itemTypeProperties);
+    //this.entity.ItemTypeProperty.push(this.itemTypeProperties);
+    //console.log(this.itemTypeProperties);
     // let validateResult: any = await this.onValidateItemTypeProperty( e.data)
     // if (!validateResult.Success)
     //   this.toastr.error('ItemTypePropertyName already exsited!', 'Error!');
@@ -63,11 +162,27 @@ export class ItemTypeComponent implements OnInit {
     //     this.dataGrid.instance.refresh();
     //   });
     // detail them ở đây
-    this.itemTypeProperties.push(e.data)// thằng con owrd đây
-    this.dataSourceProperties.push(this.itemTypeProperties);
-    console.log(this.itemTypeProperties);
+ // }
+  //async onRowUpdatingProperty(e) {
     
-  }
+    // Modify entity olddata to newdata;
+   // const data = Object.assign(e.oldData, e.newData);
+    //console.log(data);
+   // this.itemTypeProperties.map(data);
+    // push({ItemTypeId: data.ItemTypeId,
+    //   ItemTypePropertyId: data.ItemTypePropertyId,
+    //   ItemTypePropertyName: data.ItemTypePropertyName});
+    //console.log(this.itemTypeProperties);
+    // this.itemTypePropertyService.updateItemTypeProperty(data).subscribe(res => {
+    //   const result = res as any;
+    //   if (result.Success) {
+    //     this.toastr.success('Update success!', 'Success!');
+    //     // this.dataGrid.instance.refresh();
+    //   } else {
+    //     Swal.fire('Error!', result.Message, 'error');
+    //   }
+    // });
+ // }
   /**
    * Validate ItemTypeName have to not exist
    * @param e validate 2 params with ItemTypeName and ItemTypeCode
@@ -82,54 +197,34 @@ export class ItemTypeComponent implements OnInit {
    * Update ItemType
    * @param e Update with ItemType parames
    */
-  async onRowUpdatingItemType(e) {
+ //onRowUpdatingItemType(e) {
+    //console.log(this.dataSourceProperties);
     // Modify entity olddata to newdata;
-    const data = Object.assign(e.oldData, e.newData);
-    data.ModifyBy = this.auth.currentUser.Username;
-    data.Status = data.Status ? 1 : 0; //tenary operation if (data.status == true) return 1 else return 0
-    data.ItemTypeProperty = this.itemTypeProperties;// đây là khi lưu cha con nó lưu luôn
-    console.log('Save Master detail')
-    console.log(data);
-    let validateResult: any = await this.onValidateItemTypeName(data)
+    // const data = Object.assign(e.oldData, e.newData);
+    // data.ModifyBy = this.auth.currentUser.Username;
+    // data.Status = data.Status ? 1 : 0; //tenary operation if (data.status == true) return 1 else return 0
+    // data.ItemTypeProperty = this.itemTypeProperties;// đây là khi lưu cha con nó lưu luôn
+    //console.log('Save Master detail')
+    //console.log(data);
+    //let validateResult: any = await this.onValidateItemTypeName(data)
     // debugger;
-    if (!validateResult.Success)
-      this.toastr.error('ItemTypeName already exsited!', 'Error!');
-    else {
-      this.itemTypeService.updateItemType(data).subscribe(res => {
+    // if (!validateResult.Success)
+    //   this.toastr.error('ItemTypeName already exsited!', 'Error!');
+    // else {
+    //   this.itemTypeService.updateItemType(data).subscribe(res => {
 
-        const result = res as any;
-        if (result.Success) {
-          this.toastr.success('Update success!', 'Success!');
-          this.dataGrid.instance.refresh();
-        } else {
-          Swal.fire('Error!', result.Message, 'error');
-        }
-      });
-    }
+    //     const result = res as any;
+    //     if (result.Success) {
+    //       this.toastr.success('Update success!', 'Success!');
+    //       this.dataGrid.instance.refresh();
+    //     } else {
+    //       Swal.fire('Error!', result.Message, 'error');
+    //     }
+    //   });
+    //}
 
-  }
-  /**
-   * Update ItemTypeProperty
-   * @param e params for Property Detail DataGrid
-   * Only ItemTypePropetyName
-   */
-  onRowUpdatingProperty(e) {
-    // Modify entity olddata to newdata;
-    const data = Object.assign(e.oldData, e.newData);
-    this.itemTypePropertyService.updateItemTypeProperty(data).subscribe(res => {
-      const result = res as any;
-      if (result.Success) {
-        this.toastr.success('Update success!', 'Success!');
-        // this.dataGrid.instance.refresh();
-      } else {
-        Swal.fire('Error!', result.Message, 'error');
-      }
-    });
-  }đo
-  /**
-   * Remove ItemTypePropertyId
-   * @param e with ItemTypePropertyId
-   */
+  //}
+
   onRowRemovingProperty(e) {
     this.itemTypePropertyService.deleteItemTypeProperty(e.data.ItemTypeId).subscribe(res => {
       const result = res as any;
@@ -141,21 +236,8 @@ export class ItemTypeComponent implements OnInit {
       }
     });
   }
-  /**
-   * filter all itemTypeProperties with ItemTypeId in row selected
-   * @param e itemTypeId
-   */
-  filterByItemTypeId(e){
-    ////cái e này là entity na anh
-    //z là đc rồi đo
-    //
-    //cái này là của thằng cha để lấy cái itemTypeId ///
-    this.itemTypeId = e.data.ItemTypeId;
-    this.dataSourceProperties = 
-        this.itemTypePropertyService.getDataGridItemTypePropertyByItemTypeId(this.dataSourceProperties, "ItemTypePropertyId", this.itemTypeId);
-    this.entity = e.data;
-        console.log( this.entity);  
-  }
+
+ 
 
   /**
    * Init Function
@@ -165,7 +247,6 @@ export class ItemTypeComponent implements OnInit {
     e.data.Status = 1;
     e.data.CreateBy = this.auth.currentUser.Username;
   }
-
 
 
 }
