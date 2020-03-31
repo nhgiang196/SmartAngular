82c1681aef7e585fcef3a8db874152dcb02ae08f
giang.nh <giang.nh@becamex.com.vn>
2020-03-31 19:34:48 +0700

    UPDATE

--- src/app/views/pages/categories/categories.module.ts
+++ src/app/views/pages/categories/categories.module.ts
@@ -5,7 +5,7 @@ import { CategoriesRoutingModule } from './categories-routing.module';
 import { SharedModule } from 'src/app/core/shared/shared.module';
 import { LanguageService } from 'src/app/core/services/language.service';
 import { MainViewCategoryComponent } from './main-view-category/main-view-category.component';
-import { DxButtonModule, DxDataGridModule, DxSpeedDialActionModule, DxSwitchModule, DxSelectBoxModule } from 'devextreme-angular';
+import { DxButtonModule, DxDataGridModule, DxSpeedDialActionModule, DxSwitchModule, DxSelectBoxModule, DxTextBoxModule, DxCheckBoxModule, DxDateBoxModule, DxValidatorModule, DxValidationSummaryModule, DxFormModule } from 'devextreme-angular';
 
 import { ItemComponent } from './item/item.component';
 import { ItemTypeComponent } from './item-type/item-type.component';
@@ -47,6 +47,14 @@ const COMPONENTS = [
     DxSpeedDialActionModule,
     DxSwitchModule,
     DxSelectBoxModule,
+    DxSelectBoxModule,
+    DxCheckBoxModule,
+    DxTextBoxModule,
+    DxDateBoxModule,
+    DxButtonModule,
+    DxValidatorModule,
+    DxFormModule,
+    DxValidationSummaryModule,
     ModalModule.forRoot()
   ],
   declarations: [...COMPONENTS],
