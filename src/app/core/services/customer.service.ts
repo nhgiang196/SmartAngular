32bc2d73a36b0ecbda606d9eadc279a18366fd6c
giang.nh <giang.nh@becamex.com.vn>
2020-04-01 09:35:10 +0700

    update Customer Service

--- src/app/core/services/customer.service.ts
+++ src/app/core/services/customer.service.ts
@@ -18,15 +18,6 @@ export class CustomerService {
   updateCustomer =(entity) => this.http.put(`${ApiUrl}/Customer/UpdateCustomer`,entity);
   deleteCustomer =(id) => this.http.delete(`${ApiUrl}/Customer/DeleteCustomer`,{ params: { id: id } });
   getCustomerPagination =(entity) => this.http.post<any>(`${ApiUrl}/IteCustomerm/GetCustomerPagination`,entity,{} );
-  
-  
-  
-  getDataTableCustomerPagination =(entity) => { // Note: BA yêu cầu gửi Parram như thế này
-    entity.KeyFields = `c.CustomerId, c.CustomerName, c.FactoryId	,c.CustomerAddress, c.ContactName, c.ContactEmail, c.ContactPhone, c.Description, c.CreateBy	, c.CreateDate, c.ModifyBy	, c.ModifyDate, c.Status	, c.IsIntergration`;
-    entity.SelectFields = `
-     c.*, FactoryName , [CustomerStatus] = dbo.GetDefine('CustomerStatus',c.Status) `;
-    entity.Entity = `  Customer c LEFT JOIN Factory f ON f.FactoryId = c.FactoryId`;
-    return this.http.post<DataTablesResponse>(`${ApiUrl}/Customer/DataTableCustomerPagination`,entity);}
   getCustomer =() => this.http.get(`${ApiUrl}/Customer/GetCustomer` );
   findCustomerById =(id) => this.http.get<any>(`${ApiUrl}/Customer/FindCustomerById?id=${id}` );
   validateCustomer =(entity) =>{ return this.http.post(`${ApiUrl}/Customer/ValidateCustomer`,entity);}
@@ -35,10 +26,10 @@ export class CustomerService {
     var dataSource = new DataSource({
       store: AspNetData.createStore({
           loadUrl: `${ApiUrl}/Customer/GetCustomerDataGridPagination`,
-          loadParams: {key: 'CustomerId'}
       }) ,
       paginate: true,
       pageSize: 10,
+      filter: [],
       })
       return dataSource;
   }
