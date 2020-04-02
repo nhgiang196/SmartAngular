45de353f2e237d0b4e3130b44dcff7eb82fbe6d8
dung.lt.cntt <thanhtai42@gmail.com>
2020-04-02 23:39:42 +0700

    change somethings in itemType

--- src/app/core/services/item-type.service.ts
+++ src/app/core/services/item-type.service.ts
@@ -3,12 +3,14 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import * as AspNetData from "devextreme-aspnet-data-nojquery";
+import CustomStore from 'devextreme/data/custom_store';
+import { StageService } from './stage.service';
 
 const ApiUrl = environment.apiUrl;
 const NULL_ROUTES = `${environment.apiUrl}/DevExtreme/NullRoutes`
 @Injectable({ providedIn: 'root' })
 export class ItemTypeService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private stageService : StageService) {}
   addItemType = (entity) => this.http.post(`${ApiUrl}/ItemType/AddItemType`, entity);
   updateItemType = (entity) => this.http.put(`${ApiUrl}/ItemType/UpdateItemType`, entity);
   deleteItemType = (id) => this.http.delete(`${ApiUrl}/ItemType/DeleteItemType`, { params: { id: id } });
@@ -20,18 +22,28 @@ export class ItemTypeService {
   getItemTypePaginationByCode = (entity, code) => this.http.post<any>(`${ApiUrl}/ItemType/GetItemTypePaginationByCode/${code}`, entity, {});
   validateItemType = (entity) => this.http.post(`${ApiUrl}/ItemType/ValidateItemType`, entity);
 
-  getDataGridItemType(dataSource, key) {
-    dataSource = AspNetData.createStore({
-      key: key,
-      loadUrl: `${ApiUrl}/ItemType/DataGridItemTypePagination`,
-      insertUrl: NULL_ROUTES,
-      updateUrl: NULL_ROUTES,
-      deleteUrl: NULL_ROUTES,
-      onBeforeSend: (method, ajaxOptions) => {
-        ajaxOptions.data.key = key;
-        ajaxOptions.xhrFields = { withCredentials: true };
-      }
+  // getDataGridItemType(dataSource, key) {
+  //   dataSource = AspNetData.createStore({
+  //     key: key,
+  //     loadUrl: `${ApiUrl}/ItemType/DataGridItemTypePagination`,
+  //     insertUrl: NULL_ROUTES,
+  //     updateUrl: NULL_ROUTES,
+  //     deleteUrl: NULL_ROUTES,
+  //     onBeforeSend: (method, ajaxOptions) => {
+  //       ajaxOptions.data.key = key;
+  //       ajaxOptions.xhrFields = { withCredentials: true };
+  //     }
+  //   });
+  //   return dataSource;
+  // }
+
+  getDataGridItemType() {
+    return new CustomStore({
+      key: "ItemTypeId",
+      load: () => this.stageService.sendRequest(ApiUrl + "/ItemType/DataGridItemTypePagination"),
+      insert: (values) => this.addItemType(values).toPromise().then(),
+      update: (key, values) =>  this.updateItemType(values).toPromise().then(),
+      remove: (key) =>  this.deleteItemType(key).toPromise().then()
     });
-    return dataSource;
   }
 }
