1d08411560944aab0dcadf582cb08d5c1e76882b
dung.lt.cntt <thanhtai42@gmail.com>
2020-04-03 17:09:13 +0700

    create chart

--- src/app/views/pages/monitor/monitor-chart/monitor-chart.component.ts
+++ src/app/views/pages/monitor/monitor-chart/monitor-chart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FactoryService, ChartFactory } from 'src/app/core/services';
+import { FactoryService, MonitorService } from 'src/app/core/services';
+import { formatDate } from '@angular/common';
+import { Monitor, MonitorDescription, ChartFactory } from 'src/app/core/models/monitor';
 
 @Component({
   selector: 'app-monitor-chart',
@@ -15,23 +17,45 @@ export class MonitorChartComponent implements OnInit {
   colCount: number;
   width: any;
   chartFactory: ChartFactory;
-   constructor(private factoryService: FactoryService) 
-  {
+  monitorsInfo: Monitor[];
+  monitorSources: MonitorDescription[];
+  types: string[] = ["bar", "line", "stackedline", "fullstackedline"];
+  constructor(private factoryService: FactoryService, private monitorService: MonitorService) {
     this.chartFactory = new ChartFactory();
+    this.monitorSources = this.monitorService.getMonitorSources();
     this.labelLocation = "top";
     this.readOnly = false;
     this.showColon = true;
     this.minColWidth = 300;
     this.colCount = 2;
-   
+
   }
 
-   ngOnInit() {
-    // let filter = id!=0?["ItemTypeId", "=",id]:[];
-    // //this.dataSource= this.itemService.getDataGridItem('ItemId');
-    // this.dataSource.filter(filter);
-    this.factories =  this.factoryService.loadFactorySelectBox('FactoryId');
+  ngOnInit() {
+    this.factories = this.factoryService.loadFactorySelectBox('FactoryId');
     console.log(this.factories);
+  }
+  onValueChanged(e){
+    this.chartFactory.factoryId = e.value
+  }
+  chartQuery() {
+    const format = 'MM/dd/yyyy HH:mm:ss';
+    const locale = 'en-US';
+    let entity = this.chartFactory;
+    entity.dateFrom = formatDate(entity.dateFrom, format, locale);
+    entity.dateTo = formatDate(entity.dateTo, format, locale);
+    console.log(entity);
+    this.monitorService.getChartByDate(entity).toPromise().then(res =>  this.monitorsInfo = res as Monitor[]);
+    // this.monitorsInfo = await this.monitorService.getChartByDate(entity).toPromise().then();
+    // console.log(this.monitorsInfo);
+  }
+  legendClick(e: any) {
+    var series = e.target;
+    if (series.isVisible()) {
+      series.hide();
+    } else {
+      series.show();
+    }
   }
 
 }
