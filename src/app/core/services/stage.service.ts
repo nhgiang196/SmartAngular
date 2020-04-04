a92163dd43470d9349326d54bd1a7b95bcc1005e
dung.lt.cntt <thanhtai42@gmail.com>
2020-04-04 16:44:19 +0700

    update

--- src/app/core/services/stage.service.ts
+++ src/app/core/services/stage.service.ts
@@ -7,12 +7,12 @@ import DataSource from 'devextreme/data/data_source';
 import * as AspNetData from "devextreme-aspnet-data-nojquery";
 import { formatDate } from '@angular/common';
 import CustomStore from 'devextreme/data/custom_store';
+import { MyHelperService } from './my-helper.service';
 const ApiUrl = environment.apiUrl;
 
 @Injectable({ providedIn: 'root' })
 export class StageService {
-  requests: string[] = [];
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private helper: MyHelperService) {
 
   }
   addStage = (entity) => this.http.post(`${ApiUrl}/Stage/AddStage`, entity);
@@ -54,7 +54,7 @@ export class StageService {
   getDataGridStage() {
     return new CustomStore({
       key: "StageId",
-      load: () => this.sendRequest(ApiUrl + "/Stage/GetStageDataGridPagination"),
+      load: () => this.helper.sendRequest(ApiUrl + "/Stage/GetStageDataGridPagination"),
       insert: (values) => this.addStage(values).toPromise(),
       update: (key, values) => {
         console.log(values);
@@ -63,50 +63,6 @@ export class StageService {
       remove: (key) => this.deleteStage(key).toPromise().then()
     });
   }
-  sendRequest(url: string, method: string = "GET", data: any = {}): any {
-    this.logRequest(method, url, data);
-
-    let httpParams = new HttpParams({ fromObject: data });
-    let httpOptions = { withCredentials: true, body: httpParams };
-    let result;
-
-    switch (method) {
-      case "GET":
-        result = this.http.get(url, httpOptions);
-        break;
-      case "PUT":
-        result = this.http.put(url, httpParams, httpOptions);
-        break;
-      case "POST":
-        result = this.http.post(url, httpParams, httpOptions);
-        break;
-      case "DELETE":
-        result = this.http.delete(url, httpOptions);
-        break;
-    }
-
-    return result
-      .toPromise()
-      .then((data: any) => {
-        return method === "GET" ? data.data : data;
-      })
-      .catch(e => {
-        throw e && e.error && e.error.Message;
-      });
-  }
-
-  logRequest(method: string, url: string, data: object): void {
-    var args = Object.keys(data || {}).map(function (key) {
-      return key + "=" + data[key];
-    }).join(" ");
-
-    var time = new Date();
-
-    this.requests.unshift([time, method, url.slice(URL.length), args].join(" "))
-  }
-
-  clearRequests() {
-    this.requests = [];
-  }
+ 
 
 }
