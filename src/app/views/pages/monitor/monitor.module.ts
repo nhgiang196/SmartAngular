45c45a5a017da1d3278bd2801526d667fae6a29a
dung.lt.cntt <thanhtai42@gmail.com>
2020-04-06 15:44:31 +0700

    update

--- src/app/views/pages/monitor/monitor.module.ts
+++ src/app/views/pages/monitor/monitor.module.ts
@@ -4,18 +4,14 @@ import { MonitorComponent } from './monitor.component';
 import { MainViewMonitorComponent } from './main-view-monitor/main-view-monitor.component';
 import { MonitorRoutingModule } from './monitor-routing.module';
 import { MonitorChartComponent } from './monitor-chart/monitor-chart.component';
-import { MonitorFactoryDataComponent } from './monitor-factory-data/monitor-factory-data.component';
 import { MonitorStandardComponent } from './monitor-standard/monitor-standard.component';
-import { MonitorTrackingComponent } from './monitor-tracking/monitor-tracking.component';
 import { DxDataGridModule,DxSelectBoxModule, DxSwitchModule, DxSpeedDialActionModule, DxFormModule, DxDateBoxModule, DxChartModule } from 'devextreme-angular';
 import { SharedModule } from 'src/app/core/shared/shared.module';
 const COMPONENTS =[
   MonitorComponent,
   MainViewMonitorComponent,
   MonitorChartComponent,
-  MonitorFactoryDataComponent,
   MonitorStandardComponent,
-  MonitorTrackingComponent
 ]
 @NgModule({
   imports: [
