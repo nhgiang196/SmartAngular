c10f654cbd801416fc4b2375ddd880a44f931573
MrQuyenIT <vanquyenktktbd@gmail.com>
2020-04-09 16:20:49 +0700

    fix language

--- src/app/views/pages/admin/admin.component.ts
+++ src/app/views/pages/admin/admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { LanguageService } from 'src/app/core/services/language.service';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +8,9 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor(private translate: TranslateService) {
-    translate.use(localStorage.getItem('locallanguage') || 'en');
-  }
+  constructor(private translate: TranslateService,private langService:LanguageService) {
+    translate.use(langService.getLanguage());
+   }
 
   ngOnInit() {
   }
--- src/app/views/pages/process/process.component.ts
+++ src/app/views/pages/process/process.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { LanguageService } from 'src/app/core/services/language.service';
 
 @Component({
   selector: 'app-process',
@@ -7,8 +8,8 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class ProcessComponent implements OnInit {
 
-  constructor(private translate: TranslateService) {
-    translate.use(localStorage.getItem('locallanguage') || 'en');
+  constructor(private translate: TranslateService,private langService:LanguageService) {
+    translate.use(langService.getLanguage());
    }
 
   ngOnInit() {
