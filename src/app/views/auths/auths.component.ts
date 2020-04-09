55c3a9dae723b58b590f1dc9c5848e18574de8b6
MrQuyenIT <vanquyenktktbd@gmail.com>
2020-04-09 16:28:52 +0700

    fix language login

--- src/app/views/auths/auths.component.ts
+++ src/app/views/auths/auths.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { changeColorBody } from 'src/app/app.helpers';
 import { TranslateService } from '@ngx-translate/core';
+import { LanguageService } from 'src/app/core/services/language.service';
 
 @Component({
   selector: 'app-auths',
   template:` <router-outlet></router-outlet>`
 })
 export class AuthsComponent implements OnInit{
-  constructor(private translate: TranslateService) {
-    translate.use(localStorage.getItem('locallanguage') || 'en');
-  }
+  constructor(private translate: TranslateService,private langService:LanguageService) {
+    translate.use(langService.getLanguage());
+   }
 
-  
+
   ngOnInit(): void {
 
   }
