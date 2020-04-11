499fc62d7d5dba705677a98ec6c3bb6cf0567f4d
MrQuyenIT <vanquyenktktbd@gmail.com>
2020-04-11 14:00:25 +0700

    update process log

--- src/app/core/services/process-log.service.ts
+++ src/app/core/services/process-log.service.ts
@@ -5,12 +5,36 @@ import { DataTablesResponse } from '../models/datatable';
 import { environment } from 'src/environments/environment';
 import { ProcessLog } from '../models/process';
 import { GenericFactoryService } from './general/generic-factory.service';
+import DataSource from 'devextreme/data/data_source';
+import * as AspNetData from "devextreme-aspnet-data-nojquery";
 const ApiUrl = environment.apiUrl;
 @Injectable({providedIn: 'root'})
 export class ProcessLogService extends GenericFactoryService<ProcessLog>{
   constructor(http: HttpClient) {
     super(http,'ProcessLog');
   }
-  findProcessLog =(factoryId,stageId,itemOutId) => this.http.get<any>(`${ApiUrl}/ProcessLog/FindProcessLog`,{ params: { factoryId: factoryId ,stageId:stageId,itemOutId}});
+  findProcessLog =(factoryId,stageId,itemOutId,startDate,endDate) => this.http.get<any>(`${ApiUrl}/ProcessLog/FindProcessLog`,{ params: { factoryId: factoryId ,stageId:stageId,itemOutId,startDate,endDate}});
   searchProcessLog =(factoryid,endate) => this.http.get<any>(`${ApiUrl}/ProcessLog/SearchProcessLog`,{ params: { factoryid: factoryid , endDate : endate} });
+  loadDxoGridProcessLog(factoryId,stageId,itemOutId,startDate,endDate){
+    return new DataSource({
+       store:AspNetData.createStore({
+         key: "ProcessLogId",
+         loadUrl: `${ApiUrl}//ProcessLog/FindProcessLogs`,
+        //  deleteUrl:`${ApiUrl}/${entity}/${actionDelete}`,
+        //  updateUrl:`${ApiUrl}/${entity}/${actionUpdate}`,
+        //  insertUrl:`${ApiUrl}/${entity}/${actionInsert}`,
+         onBeforeSend: function (method, ajaxOptions) {
+           ajaxOptions.data.keyId ="ProcessLogId";
+        },
+        loadParams:{
+          factoryId,
+          stageId,
+          itemOutId,
+          startDate,
+          endDate
+        }
+       }),
+       filter:["Status","=","1"]
+     });
+   }
 }
--- src/app/views/pages/process/process-log/process-log.component.ts
+++ src/app/views/pages/process/process-log/process-log.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ProcessLog } from 'src/app/core/models/process';
 import { ProcessLogService } from 'src/app/core/services';
 import { MyHelperService } from 'src/app/core/services/my-helper.service';
-import { BomFactory } from 'src/app/core/models/bom';
+import { BomFactory, BomStage } from 'src/app/core/models/bom';
 import { async } from 'rxjs/internal/scheduler/async';
+import { DevextremeService } from 'src/app/core/services/general/devextreme.service';
+import { DxDataGridComponent } from 'devextreme-angular';
 
 @Component({
   selector: 'app-process-log',
@@ -11,34 +13,57 @@ import { async } from 'rxjs/internal/scheduler/async';
   styleUrls: ['./process-log.component.css']
 })
 export class ProcessLogComponent implements OnInit {
+  @ViewChild("dataGridVar",{static:false}) dataGrid: DxDataGridComponent;
   //entity: ProcessLog = new ProcessLog();
   startDay:Date
   endDay: Date
   factoryId:number
   bomFactory: BomFactory
+  dataSourceProcessLog: any;
+  dataSourceFactory:any;
+  dataSourceStage:any;
+  dataSourceItem:any;
+  dataSourceUnit:any;
+  dataSourceShift:any;
 
   bsConfig = { dateInputFormat: "YYYY-MM-DD", adaarptivePosition: true };
-  constructor(private processLogService: ProcessLogService,private helper: MyHelperService) { }
+  constructor(private processLogService: ProcessLogService,private devExtreme: DevextremeService,private helper: MyHelperService) { }
 
   ngOnInit() {
     this.bomFactory = new BomFactory();
+    this.dataSourceFactory= this.devExtreme.loadDxoLookup("Factory");
+    this.dataSourceStage= this.devExtreme.loadDxoLookup("Stage");
+    this.dataSourceItem= this.devExtreme.loadDxoLookup("Item");
+    this.dataSourceUnit= this.devExtreme.loadDxoLookup("Unit");
+    this.dataSourceShift= this.devExtreme.loadDxoLookup("Shift");
   }
 
   async fnFindBomFactoryId(){
-    let endDay = this.helper.dateConvertToString(this.endDay)
-    var dataBomFactory =await  this.processLogService.searchProcessLog(this.factoryId,endDay).toPromise().then();
+    let startDate = this.helper.dateConvertToString(this.startDay)
+    let endDate = this.helper.dateConvertToString(this.endDay)
+    var dataBomFactory =await  this.processLogService.searchProcessLog(this.factoryId,endDate).toPromise().then();
     this.bomFactory = dataBomFactory;
     if(this.bomFactory!=null&&this.bomFactory.BomStage.length>0&& this.bomFactory.BomStage[0].BomItemOut.length>0){
       let stage =  this.bomFactory.BomStage[0];
       let itemOut = this.bomFactory.BomStage[0].BomItemOut[0];
-      this.findProcessLog(this.factoryId,stage.StageId,itemOut.ItemId).subscribe(res=>{
-        console.log(res);
-      });
+      this.dataSourceProcessLog =  this.processLogService.loadDxoGridProcessLog(this.factoryId,stage.StageId,itemOut.ItemId,startDate,endDate);
     }
   }
 
-  findProcessLog(factoryId,stageId,itemOutId){
-   return this.processLogService.findProcessLog(factoryId,stageId,itemOutId);
+  loadProcessLogByItemOut(stageId , itemId){
+    let startDate = this.helper.dateConvertToString(this.startDay)
+    let endDate = this.helper.dateConvertToString(this.endDay)
+    this.dataSourceProcessLog =  this.processLogService.loadDxoGridProcessLog(this.factoryId,stageId,itemId,startDate,endDate);
+  }
+
+  loadProcessLogByStage(stage: BomStage){
+    let startDate = this.helper.dateConvertToString(this.startDay)
+    let endDate = this.helper.dateConvertToString(this.endDay)
+    if(stage.BomItemOut.length>0){
+      let itemOut = stage.BomItemOut[0];
+      this.dataSourceProcessLog =  this.processLogService.loadDxoGridProcessLog(this.factoryId,stage.StageId,itemOut.ItemId,startDate,endDate);
+    }
+
   }
 
 }
