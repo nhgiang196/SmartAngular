420384efd6b35d462dcf4490b9c8111cad362701
giang.nh <giang.nh@becamex.com.vn>
2020-04-16 10:17:40 +0700

    500.000 points highchart

--- src/app/views/pages/monitor/monitor-chart/monitor-chart.component.ts
+++ src/app/views/pages/monitor/monitor-chart/monitor-chart.component.ts
@@ -81,43 +81,34 @@ export class MonitorChartComponent implements OnInit {
     };
 
     this.options = {
+
       chart: {
-        type: 'scatter',
-        height: 700
+          zoomType: 'x'
       },
+  
       title: {
-        text: 'Sample Scatter Plot'
+          text: 'Highcharts drawing ' + 500000 + ' points'
       },
-      credits: {
-        enabled: false
+  
+      subtitle: {
+          text: 'Using the Boost module'
       },
+  
       tooltip: {
-        formatter: function () {
-          return '<b>x: </b>' + Highcharts.dateFormat('%e %b %y %H:%M:%S', this.x) +
-            ' <br> <b>y: </b>' + this.y.toFixed(2);
-        }
+          valueDecimals: 2
       },
+  
       xAxis: {
-        type: 'datetime',
-        labels: {
-          formatter: function () {
-            return Highcharts.dateFormat('%e %b %y', this.value);
-          }
-        }
+          type: 'datetime'
       },
-      series: [
-        {
-          name: 'Normal',
-          turboThreshold: 500000,
-          data: [[new Date('2018-01-25 18:38:31').getTime(), 2]]
-        },
-        {
-          name: 'Abnormal',
-          turboThreshold: 500000,
-          data: [[new Date('2018-02-05 18:38:31').getTime(), 7]]
-        }
-      ]
-    }
+  
+      series: [{
+          data: this.getData(500000),
+          lineWidth: 0.5,
+          name: 'Hourly data points'
+      }]
+  
+  }
 
 
 
@@ -131,6 +122,43 @@ export class MonitorChartComponent implements OnInit {
     console.log(this.factories);
     Highcharts.chart('container', this.options);
   }
+
+  getData(n) {
+    var arr = [],
+        i,
+        x,
+        a,
+        b,
+        c,
+        spike;
+    for (
+        i = 0, x = Date.UTC(new Date().getUTCFullYear(), 0, 1) - n * 36e5;
+        i < n;
+        i = i + 1, x = x + 36e5
+    ) {
+        if (i % 100 === 0) {
+            a = 2 * Math.random();
+        }
+        if (i % 1000 === 0) {
+            b = 2 * Math.random();
+        }
+        if (i % 10000 === 0) {
+            c = 2 * Math.random();
+        }
+        if (i % 50000 === 0) {
+            spike = 10;
+        } else {
+            spike = 0;
+        }
+        arr.push([
+            x,
+            2 * Math.sin(i / 100) + a + b + c + spike + Math.random()
+        ]);
+    }
+    return arr;
+}
+
+
 
   onValueChanged(e) {
     this.chartFactory.factoryId = e.value
