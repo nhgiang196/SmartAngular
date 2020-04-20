7ece537666f0a6886bad5ce7febadb93a40bea3e
giang.nh <giang.nh@becamex.com.vn>
2020-04-20 15:35:41 +0700

    mask Service not check

--- src/app/views/pages/admin/admin.module.ts
+++ src/app/views/pages/admin/admin.module.ts
@@ -11,6 +11,9 @@ import { FormsModule } from '@angular/forms';
 import { DxiItemModule } from 'devextreme-angular/ui/nested';
 import { DxFormModule } from 'devextreme-angular';
 
+import { NgxMaskModule, IConfig } from 'ngx-mask'
+
+
 const routes: Routes = [{
   path: '',
   component: AdminComponent,
@@ -38,6 +41,7 @@ const routes: Routes = [{
     FormsModule,
     DxiItemModule,
     DxFormModule,
+    NgxMaskModule.forRoot()
   ],
   exports: [RouterModule, FactoryComponent, AdminComponent, MainViewAdminComponent],
   
